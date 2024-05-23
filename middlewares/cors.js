25578cb8b0ba315f0507635790a519916696ed88
outcast <vx4749720@gmail.com>
2024-05-23 21:36:06 +0300

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -4,7 +4,8 @@ const allowedCors = [
     'http://localhost:3000',
     'http://localhost:3001',
     // 'https://outcast.nomoredomainswork.ru',
-    'https://outcastx0.nomoredomainswork.ru/',
+    'https://outcastx0.nomoredomainswork.ru',
+    'outcastx0.nomoredomainswork.ru'
 ];
 
 function cors(req, res, next) {
