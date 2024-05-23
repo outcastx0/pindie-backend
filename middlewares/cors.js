49fed81175cbe4d415101abb9025a6f39859be32
outcast <vx4749720@gmail.com>
2024-05-23 20:15:48 +0300

    cors removed

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,21 +1,36 @@
-const allowedCors = [
-    'https://practicum.yandex.ru',
-    'https://students-projects.ru',
-    'http://localhost:3000',
-    'http://localhost:3001',
-    // 'https://outcast.nomoredomainswork.ru',
-    'https://outcastx0.nomoredomainswork.ru',
-];
+const allowedCors = ["https://pindie-frontend.nomoredomainswork.ru"];
 
 function cors(req, res, next) {
-    const { origin } = req.headers;
+  const { origin } = req.headers;
 
-    if (allowedCors.includes(origin)) {
-        res.header('Access-Control-Allow-Origin', origin);
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-        res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
-    }
-    next();
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  next();
 }
 
-module.exports = { cors };
+module.exports = cors;
+
+// const allowedCors = [
+//     'https://practicum.yandex.ru',
+//     'https://students-projects.ru',
+//     'http://localhost:3000',
+//     'http://localhost:3001',
+//     // 'https://outcast.nomoredomainswork.ru',
+//     'https://outcastx0.nomoredomainswork.ru',
+// ];
+
+// function cors(req, res, next) {
+//     const { origin } = req.headers;
+
+//     if (allowedCors.includes(origin)) {
+//         res.header('Access-Control-Allow-Origin', origin);
+//         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//         res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+//     }
+//     next();
+// }
+
+// module.exports = { cors };
