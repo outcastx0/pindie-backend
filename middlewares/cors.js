50bb18636aff4ba1a5c86d8c4d156c0d078ca048
outcast <vx4749720@gmail.com>
2024-05-23 19:55:34 +0300

    headers added

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -13,7 +13,7 @@ function cors(req, res, next) {
     if (allowedCors.includes(origin)) {
         res.header('Access-Control-Allow-Origin', origin);
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'Content-type,Authorization,Accept,X-Custom-Header');
+        res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     }
     next();
 }
