6fa22c154e56bc6177716a38e74c349f14cabc78
outcast <vx4749720@gmail.com>
2024-05-24 18:03:42 +0300

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,6 +2,7 @@ const allowedCors = [
     "https://practicum.yandex.ru",
     "https://students-projects.ru",
     "http://localhost:3000",
+    "https://outcast.nomoredomainswork.ru",
     "https://outcastx0.nomoredomainswork.ru",
 ];
 
@@ -14,13 +15,6 @@ function cors(req, res, next) {
     res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     next();
-
-    // if (allowedCors.includes(origin)) {
-    //     res.header('Access-Control-Allow-Origin', origin);
-    //     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    //     res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
-    // }
-    // next();
 }
 
 module.exports = { cors };
