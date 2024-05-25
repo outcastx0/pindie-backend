18aaf8ebdf47798482ace74463b55d9fbf191c70
outcast <vx4749720@gmail.com>
2024-05-25 11:18:55 +0300

    registration added

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,7 @@ const usersRouter = require('express').Router();
 const { findAllUsers, createUser, findUserById, updateUser, deleteUser, checkEmptyNameAndEmailAndPassword, checkEmptyNameAndEmail, checkIsUserExists,
     hashPassword, checkIsRegisterRequest } = require('../middlewares/users');
 const { sendAllUsers, sendUserCreated, sendUserById, sendUserUpdated, sendUserDeleted, sendMe } = require('../controllers/users');
-// const { register } = require('../controllers');
 const { checkAuth } = require('../middlewares/auth');
-const { login } = require('../controllers/auth');
 
 usersRouter.post('/users', findAllUsers, checkIsRegisterRequest ,checkIsUserExists, checkEmptyNameAndEmailAndPassword, checkAuth, hashPassword, createUser, sendUserCreated);
 usersRouter.get('/users', findAllUsers, sendAllUsers);
